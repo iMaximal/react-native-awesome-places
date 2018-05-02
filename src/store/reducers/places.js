db4694738328a9a77f53e2dcc03461548cb0cc2f
Maksim Golitsyn <maxim@infofree.ru>
2018-05-02 17:04:24 +0500

    Updating Redux: Find Place and Add place - work

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,4 +1,4 @@
-import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+import { ADD_PLACE, DELETE_PLACE } from '../actions/actionTypes';
 
 const initialState = {
   places: [],
@@ -23,20 +23,6 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         places: state.places.filter((place) => place.key !== state.selectedPlace.key),
-        selectedPlace: null,
-      };
-
-    case SELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: state.places.find(place => {
-          return place.key === action.placeKey;
-        }),
-      };
-
-    case DESELECT_PLACE:
-      return {
-        ...state,
         selectedPlace: null,
       };
 
