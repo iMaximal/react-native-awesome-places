88c57121bdf12ee48ff6ff8d0955bacd9f83dc63
Maksim Golitsyn <maxim@infofree.ru>
2018-05-06 18:19:44 +0500

    Loading Different Icons for Different Platforms

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { deletePlace } from '../../store/actions/index';
@@ -27,7 +34,11 @@ class PlaceDetail extends Component {
         <View style={ styles.deleteButton }>
           <TouchableOpacity onPress={ this.placeDeletedHandler }>
             <View>
-              <Icon size={ 30 } name="ios-trash" color="red"/>
+              <Icon
+                size={ 30 }
+                name={ Platform.OS === 'android' ? 'md-trash' : 'ios-trash' }
+                color="red"
+              />
             </View>
           </TouchableOpacity>
         </View>
