79571c10558c19f80659566467a552b65357c0eb
Maksim Golitsyn <maxim@infofree.ru>
2018-05-15 21:55:30 +0500

    Fix bug in SharePlace: input for the place might not scroll up when typing

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet, ScrollView, Image } from 'react-native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { addPlace } from '../../store/actions/index';
 import PlaceInput from '../../components/PlaceInput/PlaceInput';
@@ -67,7 +73,10 @@ class SharePlaceScreen extends Component {
   render() {
     return (
       <ScrollView>
-        <View style={ styles.container }>
+        <KeyboardAvoidingView
+          behavior='padding'
+          style={ styles.container }
+        >
           <MainText>
             <HeadingText>Share a Place with us!</HeadingText>
           </MainText>
@@ -84,7 +93,7 @@ class SharePlaceScreen extends Component {
               disabled={!this.state.controls.placeName.valid}
             />
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </ScrollView>
     );
   }
