8c549a7f033f5d1df20ae0cab6546402892e60c7
Maksim Golitsyn <maxim@infofree.ru>
2018-05-19 16:22:30 +0500

    Storing Data in Firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName,
-    location,
-    image,
+  return (dispatch) => {
+    const placeData = {
+      name: placeName,
+      location,
+    };
+    fetch('https://react-native-awe-1526404146569.firebaseio.com/places.json', {
+      method: 'POST',
+      body: JSON.stringify(placeData),
+    })
+      .catch((error) => console.log(error))
+      .then((res) => res.json())
+      .then((parsedRes) => {
+        console.log(parsedRes);
+      });
   };
 };
 
