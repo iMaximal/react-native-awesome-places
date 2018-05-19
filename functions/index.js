3c576d6e8f37c4eabeef876679ebfe856695252f
Maksim Golitsyn <maxim@infofree.ru>
2018-05-19 17:03:58 +0500

    Storing Images

---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});
+const fs = require('fs');
+const UUID = require('uuid-v4');
+
+const gcconfig = {
+  projectId: 'react-native-awe-1526404146569',
+  keyFilename: 'key.json',
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.storeImage = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const body = JSON.parse(request.body);
+    fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', (error) => {
+      console.log(error);
+      return response.status(500).json({ error });
+    });
+    const bucket = gcs.bucket('react-native-awe-1526404146569.appspot.com');
+    const uuid = UUID();
+
+    bucket.upload('/tmp/uploaded-image.jpg', {
+      uploadType: 'media',
+      destination: '/places/' + uuid + '.jpg',
+      metadata: {
+        contentType: 'image/jpeg',
+        firebaseStorageDownloadToken: uuid,
+      },
+    });
+  });
+
+  response.send("Hello from Firebase!");
+});
